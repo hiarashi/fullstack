cc26dcb314662521d81ead63fb54684a74633331
Henri Salonen <henri.t.salonen@gmail.com>
2022-12-03 13:39:32 +0200

    Up to 4.2 done

---
+++ osa4/bloglist/controllers/blogs.js
@@ -0,0 +1,29 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+const logger = require('../utils/logger')
+
+blogRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+    .catch((error) => {
+      logger.error(error.message)
+    })
+})
+
+blogRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch((error) => {
+      logger.error(error.message)
+    })
+})
+
+module.exports = blogRouter
